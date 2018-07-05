80e54a84292cbadf4c18d4dc77b0e03e1c75f182
kjoewill@gmail.com <kjoewill@gmail.com>
2018-07-05 03:29:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function sayHiToGrandma(string) {
     return "YES INDEED!"
   }
  else if (string === "I love you, Grandma.") {
-    return "YES INDEED!"
+    return "I love you, too."
   }
   else return ""
 }
